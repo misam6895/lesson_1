954ceb38dd5c21c5756e2b6d9e245ed83eb0887a
misam6895 <misam6895@gmail.com>
2024-02-15 18:58:26 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Column'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Center(
+                  child: Text('Строка 1', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Center(
+                  child: Text('Строка 2', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Center(
+                  child: Text('Строка 3', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Center(
+                  child: Text('Строка 4', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Center(
+                  child: Text('Строка 5', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
